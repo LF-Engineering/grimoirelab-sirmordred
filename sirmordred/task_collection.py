21dcaf290f73c66f26dd28367c5f5b65832248e6
paul brown <paul@whatstherub.com>
2019-04-17 12:46:28 -0700

    bump

--- sirmordred/task_collection.py
+++ sirmordred/task_collection.py
@@ -301,11 +301,13 @@ class TaskRawDataArthurCollection(Task):
         """
 
         backend_args = self._compose_arthur_params(self.backend_section, repo)
+        
         if self.backend_section == 'git':
             transformed_repo = repo.replace("https://","").replace("http://","").replace("/", "__")
             transformed_gitpath = os.path.join(self.REPOSITORY_DIR, transformed_repo)
-
-            backend_args['git_path'] = transformed_gitpath
+            
+            backend_args['git_path'] = backend_args['gitpath'] = transformed_gitpath
+        
         backend_args['tag'] = self.backend_tag(repo)
 
         ajson = {"tasks": [{}]}
