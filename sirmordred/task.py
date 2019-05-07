9d793501e197a5913351f4998b00af8dc5737bd5
paul brown <paul@whatstherub.com>
2019-05-06 19:18:59 -1000

    arthur_delay and enrich_interval are not backend_args

--- sirmordred/task.py
+++ sirmordred/task.py
@@ -38,7 +38,7 @@ class Task():
 
     NO_BACKEND_FIELDS = ['enriched_index', 'raw_index', 'es_collection_url',
                          'collect', 'pair-programming', 'fetch-archive', 'studies',
-                         'node_regex']
+                         'node_regex', 'arthur_delay', 'enrich_interval']
     PARAMS_WITH_SPACES = ['blacklist-jobs']
 
     def __init__(self, config):
